635cc1b5840df8fed555854f5f138c02715bc9cf
Carlos Alvarado <pt002014388@idat.edu.pe>
2021-08-06 15:34:51 -0500

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -5,7 +5,12 @@ def main():
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum +=roll
-    print(f'has lanzado un {roll}')
+    if roll == 1:
+      print(f'!Has lanzado un {roll} ! Critical Fail')
+    elif  roll  ==  6 :
+     print ( f'Has obtenido una { roll} ! ¡Éxito crítico!' )
+    else:
+      print(f'!Has lanzado un {roll} ')
   print(f'Has obtenido un total de {dice_sum}')
     
 
